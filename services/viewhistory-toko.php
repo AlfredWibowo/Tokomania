5d212a9a0886e3a8178dbbccef5c4a239722a09f
AlfredWibowo <75579330+AlfredWibowo@users.noreply.github.com>
2020-12-18 22:37:32 +0700

    history toko

---
+++ services/viewhistory-toko.php
@@ -0,0 +1,29 @@
+<?php
+    include "./database.php";
+    header("Content-Type: application/json");
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $username = $_POST['usernametoko'];
+        $sql = "SELECT * FROM detail_pembelian dp JOIN item i ON dp.id_item = i.id_item JOIN toko t ON i.id_toko = t.id_toko WHERE t.nama_toko = ? AND dp.status = ?;
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$username,1]);
+
+        $data = array();
+
+        while($row = $stmt->fetch())
+        {
+            array_push($data,$row);
+        }
+
+        echo json_encode($data);
+    }
+    else
+    {
+        header("HTTP/1.1 400 Bad Request");
+        $error = array(
+            'error' => 'Method not Allowed'
+        );
+
+        echo json_encode($error);
+    }
+?>
---
+++ toko/history-toko.php
@@ -0,0 +1,84 @@
+<?php
+    include "./services/database.php";
+    if(!isset($_SESSION['username']))
+    {
+        header("Location: index.php");
+        exit();
+    } 
+    $username = $_SESSION['usernametoko'];
+?>
+<html>
+    <head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+        <script src="https://use.fontawesome.com/504410ced2.js"></script>
+        <link rel="stylesheet" href="./css/home.css">
+        <link rel="stylesheet" href="https://cdnjs.cloud
+        flare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script>  
+            function History()
+            {
+                var pembeli = $("#pembeli").val();
+                $.ajax({
+                    url: "../services/viewhistory-toko.php",
+                    method: "POST",
+                    data: {
+                        username: pembeli
+                    },
+                    success: function(res){
+                        console.log(res);
+                         $("#item-list").html('');
+                         var table = $("<table class='table'></table>");
+                         var title = $("<thead><tr><td>Id Pembelian</td><td>Tanggal Pembelian</td></tr></thead>");
+                         table.append(title);
+                         res.forEach(function(item){
+                            var html = $(`
+                                <tr>
+                                <td>`+ item['id_pembelian'] +`</td>
+                                <td>` + item['tanggal'] + `</td>
+                                </tr>
+                            `)                           
+                            table.append(html);
+                         });
+                         $("#item-list").append(table);
+                    }
+                });
+            }         
+            function LogOut()
+            {
+                $.ajax({
+                    url: "./services/logout.php",
+                    method: "GET",
+                    success: function(res){
+                        if(res == "logout"){
+                            location.reload();
+                        }
+                    }
+                })
+            }
+        </script>
+    </head>
+    <body onload="History()">
+        <div class="container">
+            <div class="menu">
+                <ul>
+                    <li class="logo"><img src="toped.png"></li>
+                    <li class="active"><a href="home.php">Home</a></li>
+                    <li>Cart</li>
+                    <li><a href="search.php">Search</a></li>
+                    <li><a href="history-toko.php">History</a></li>                    
+                </ul>
+                <div class="Logout">
+                    <a href="#" class="signup-btn" onclick="LogOut()">Log Out</a>
+                </div>
+            </div>
+            <input type="hidden" id="pembeli" value="<?php echo $username; ?>"></input>
+            <div id="item-list" class="item-list">
+
+            </div>
+        </div>
+    </body>
+</html>
